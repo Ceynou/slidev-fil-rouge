4d0b7a279c4c8977476531af692f78bd9fac457f
Ceynou <saruwatari.atsushi@live.fr>
2025-05-13 15:06:44 +0200

    idk?

--- setup/setup.ts
+++
@@ -1,7 +0,0 @@
-// main/setup.ts
-import { defineAppSetup } from "@slidev/types";
-import naive from "slidev-naive-addon";
-
-export default defineAppSetup(({ app, router }) => {
-  app.use(naive);
-});
